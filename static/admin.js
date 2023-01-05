bb2de32b49c46afba7eb88221689a9bd819306a1
Nicholas Padmanabhan <nicholaspad@gmail.com>
2023-01-05 23:44:54 +0200

    Move into document.ready

--- static/admin.js
+++ static/admin.js
@@ -1,26 +1,32 @@
-EMAIL_TYPE_SELECTOR = $("#email-type-selector");
-EMAIL_SUBJECT_INPUT = $("#email-subject-input");
-EMAIL_BODY_INPUT = $("#email-body-input");
+setupEmailTemplateForm = () => {
+  EMAIL_TYPE_SELECTOR = $("#email-type-selector");
+  EMAIL_SUBJECT_INPUT = $("#email-subject-input");
+  EMAIL_BODY_INPUT = $("#email-body-input");
 
-updateEmailTemplateForm = (data, newType) => {
-  ({ body, subject } = data[newType]);
-  EMAIL_SUBJECT_INPUT.val(subject);
-  EMAIL_BODY_INPUT.val(body);
+  updateEmailTemplateForm = (data, newType) => {
+    ({ body, subject } = data[newType]);
+    EMAIL_SUBJECT_INPUT.val(subject);
+    EMAIL_BODY_INPUT.val(body);
+  };
+
+  $.get("get_email_templates", (res) => {
+    data = res.res;
+    types = Object.keys(data);
+    typeSelector = EMAIL_TYPE_SELECTOR;
+    types.forEach((type) => {
+      typeSelector.append(`<option value="${type}">${type}</option>`);
+    });
+    updateEmailTemplateForm(data, types[0]);
+    $("#email-submit").prop("disabled", false);
+  }).done((res) => {
+    data = res.res;
+    EMAIL_TYPE_SELECTOR.change(() => {
+      newType = EMAIL_TYPE_SELECTOR.val();
+      updateEmailTemplateForm(data, newType);
+    });
+  });
 };
 
-$.get("get_email_templates", (res) => {
-  data = res.res;
-  types = Object.keys(data);
-  typeSelector = EMAIL_TYPE_SELECTOR;
-  types.forEach((type) => {
-    typeSelector.append(`<option value="${type}">${type}</option>`);
-  });
-  updateEmailTemplateForm(data, types[0]);
-  $("#email-submit").prop("disabled", false);
-}).done((res) => {
-  data = res.res;
-  EMAIL_TYPE_SELECTOR.change(() => {
-    newType = EMAIL_TYPE_SELECTOR.val();
-    updateEmailTemplateForm(data, newType);
-  });
+$(document).ready(() => {
+  setupEmailTemplateForm();
 });
