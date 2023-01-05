8f72d6da2360f9e710d06bb16cf495677def61cb
Nicholas Padmanabhan <nicholaspad@gmail.com>
2023-01-05 23:43:24 +0200

    Dynamically update form selector and text inputs

--- static/admin.js
+++ static/admin.js
@@ -1,1 +1,26 @@
-console.log($("#email-type-selector").val());
+EMAIL_TYPE_SELECTOR = $("#email-type-selector");
+EMAIL_SUBJECT_INPUT = $("#email-subject-input");
+EMAIL_BODY_INPUT = $("#email-body-input");
+
+updateEmailTemplateForm = (data, newType) => {
+  ({ body, subject } = data[newType]);
+  EMAIL_SUBJECT_INPUT.val(subject);
+  EMAIL_BODY_INPUT.val(body);
+};
+
+$.get("get_email_templates", (res) => {
+  data = res.res;
+  types = Object.keys(data);
+  typeSelector = EMAIL_TYPE_SELECTOR;
+  types.forEach((type) => {
+    typeSelector.append(`<option value="${type}">${type}</option>`);
+  });
+  updateEmailTemplateForm(data, types[0]);
+  $("#email-submit").prop("disabled", false);
+}).done((res) => {
+  data = res.res;
+  EMAIL_TYPE_SELECTOR.change(() => {
+    newType = EMAIL_TYPE_SELECTOR.val();
+    updateEmailTemplateForm(data, newType);
+  });
+});
